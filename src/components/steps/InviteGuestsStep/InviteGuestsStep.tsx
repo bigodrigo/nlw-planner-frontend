6d9f9abf7b2398a625a2a0031776c8d1e7a8090c
Bigodrigo <roesbo1@hotmail.com>
2024-07-16 16:23:01 -0300

    Aula 02 - 48min

---
+++ src/components/steps/InviteGuestsStep/InviteGuestsStep.tsx
@@ -0,0 +1,32 @@
+import { ArrowRight, UserRoundPlus } from "lucide-react";
+
+interface InviteGuestsStepProps {
+    emailsToInvite: string[]
+    openConfirmTripModal: () => void
+    openGuestsModal: () => void
+}
+export default function InviteGuestsStep({
+    emailsToInvite,
+    openConfirmTripModal,
+    openGuestsModal,
+}: InviteGuestsStepProps) {
+    return (
+        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+            <button type="button" onClick={openGuestsModal} className="flex items-center gap-2 flex-1">
+            <UserRoundPlus className="size-5 text-zinc-400" />
+            {emailsToInvite.length > 0 ? (
+                <span className="text-zinc-100 text-lg flex-1 text-left">{emailsToInvite.length} pessoa(s) confidada(s)</span>
+            ) : (
+                <span className="text-zinc-400 text-lg flex-1 text-left">Quem estará na viagem?</span>
+            )}
+            </button>
+
+            <div className="w-px h-6 bg-zinc-800"/>
+
+            <button onClick={openConfirmTripModal}className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+                Confirmar viagem
+                <ArrowRight className="text-lime-950 size-5"/>
+            </button>
+        </div>
+    )
+}
